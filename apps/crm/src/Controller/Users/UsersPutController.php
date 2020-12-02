b695a9ed4818c40b2bb19b22db7b39b1354753bc
Roger Guasch <roger.guasch.rion@gmail.com>
2020-12-02 20:29:16 +0100

    launch phpunit in pre-push hook

--- apps/crm/src/Controller/Users/UsersPutController.php
+++ apps/crm/src/Controller/Users/UsersPutController.php
@@ -6,26 +6,24 @@ namespace RGR\Apps\Crm\Controller\Users;
 
 use RGR\Apps\Crm\Controller\Users\Request\CreateUserRequest;
 use RGR\Crm\Users\Application\UserCreator;
-use RGR\Shared\Domain\UuidGenerator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Uid\Uuid;
 
 final class UsersPutController
 {
 
     private UserCreator $userCreator;
-    private UuidGenerator $uuidGenerator;
 
-    public function __construct(UserCreator $userCreator, UuidGenerator $uuidGenerator)
+    public function __construct(UserCreator $userCreator)
     {
         $this->userCreator = $userCreator;
-        $this->uuidGenerator = $uuidGenerator;
     }
 
     public function __invoke(Request $request): Response
     {
         $createUserRequest = new CreateUserRequest(
-            $this->uuidGenerator->generate(),
+            Uuid::v4()->toRfc4122(),
             $request->get('name'),
             $request->get('email'),
         );
